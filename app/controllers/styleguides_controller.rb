383403d51d87a13792c1c544b2fb41e670661497
Yurii Mochalov <ym@anahoret.com>
2018-01-22 14:37:12 +0200

    Use REST

--- app/controllers/styleguides_controller.rb
+++ app/controllers/styleguides_controller.rb
@@ -1,5 +1,5 @@
-class PagesController < ActionController::Base
-  def help
+class StyleguidesController < ActionController::Base
+  def show
     # put any code here that you need
     # (although for a static view you probably won't have any)
   end
