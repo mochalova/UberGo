b9341eb010aeefaaee17dd816edbb72f596cd069
Yurii Mochalov <ym@anahoret.com>
2018-01-22 15:17:37 +0200

    Use individual layout for styleguide page

--- app/controllers/styleguides_controller.rb
+++ app/controllers/styleguides_controller.rb
@@ -1,4 +1,6 @@
 class StyleguidesController < ActionController::Base
+  layout 'layouts/styleguide'
+
   def show
     # put any code here that you need
     # (although for a static view you probably won't have any)
