45aceddbd3f6258a6c67e7c1504d1287d5e3a802
hsher <hshessar@gmail.com>
2018-02-01 20:16:51 +0200

    Use correct layout

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,6 @@
 class DashboardsController < ActionController::Base
+  layout 'layouts/application'
+
   def show
     # put any code here that you need
     # (although for a static view you probably won't have any)
