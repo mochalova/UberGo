ed1b0e5d753a8a3e495dfaf4fd92a9983f950dbb
hsher <hshessar@gmail.com>
2018-02-27 19:40:03 +0200

    Typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :styleguide, only: :show
-  resource :dashboard, only: :show
+  resource :dashboards, only: :show
   resource :conditions, only: :show
   resource :abouts, only: :show
   root 'dashboards#show'
