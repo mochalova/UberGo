6d5113a5efe127327830926664105312f5a2e3cc
hsher <hshessar@gmail.com>
2018-03-12 21:18:21 +0200

    Add remaning fields

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -11,6 +11,16 @@ class DriversController < ActionController::Base
   private
 
   def driver_params
-    params.require(:driver).permit(:last_name, :first_name, :phone, :email, :bank, :city, driver_license_front_attributes: [:file], driver_license_back_attributes: [:file])
+    params.require(:driver).permit(:last_name,
+                                   :first_name,
+                                   :phone,
+                                   :email,
+                                   :bank,
+                                   :city,
+                                   driver_license_front_attributes: [:file],
+                                   driver_license_back_attributes: [:file],
+                                   driver_tech_front_attributes: [:file],
+                                   driver_tech_back_attributes: [:file],
+                                   driver_osago_attributes: [:file])
   end
 end
